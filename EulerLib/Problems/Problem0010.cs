95749b84e18796c95b71258a89a3a8717ee550ff
Ian Nelson <ian@ianfnelson.com>
2014-05-17 07:32:47 +0100

    Problem 10

---
+++ EulerLib/Problems/Problem0010.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using EulerLib.Sequences;
+
+namespace EulerLib.Problems
+{
+    public class Problem0010 : IProblem
+    {
+        public int Id { get { return 10; } }
+        public string Title { get { return "Summation of Primes"; } }
+
+        public string Solve()
+        {
+            return SumOfPrimesBelow(2000000).ToString();
+        }
+
+        public long SumOfPrimesBelow(int ceiling)
+        {
+            return new PrimeNumbers().GenerateToMaximumValue(ceiling - 1).Sum();
+        }
+    }
+}
---
+++ EulerLibTests/Problems/Problem0010Fixture.cs
@@ -0,0 +1,20 @@
+﻿using EulerLib.Problems;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EulerLibTests.Problems
+{
+    [TestFixture]
+    public class Problem0010Fixture
+    {
+        [Test]
+        public void SumOfPrimesBelow10Is17()
+        {
+            var sut = new Problem0010();
+
+            var result = sut.SumOfPrimesBelow(10);
+
+            result.Should().Be(17);
+        }
+    }
+}
