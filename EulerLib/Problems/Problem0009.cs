7f5abdb13d3c2d5e6840ad38590d3271e2462131
Ian Nelson <ian@ianfnelson.com>
2014-05-17 23:51:29 +0100

    Problem 9

---
+++ EulerLib/Problems/Problem0009.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EulerLib.Problems
+{
+    public class Problem0009 : IProblem
+    {
+        public int Id
+        {
+            get { return 9; }
+        }
+
+        public string Title
+        {
+            get { return "Special Pythagorean triplet"; }
+        }
+
+        public string Solve()
+        {
+            for (int a = 1; a < 999; a++)
+            {
+                for (int b = a; a + b < 1000; b++)
+                {
+                    var potentialC = 1000 - a - b;
+
+                    if ((a * a) + (b * b) == (potentialC * potentialC))
+                    {
+                        return (a * b * potentialC).ToString();
+                    }
+                }
+            }
+
+            return "failed";
+        }
+    }
+}
