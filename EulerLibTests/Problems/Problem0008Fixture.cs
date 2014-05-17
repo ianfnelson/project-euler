8ba543cad4d7e3a018262dd71144b9b16844b676
Ian Nelson <ian@ianfnelson.com>
2014-05-17 23:32:26 +0100

    Problem 8

---
+++ EulerLibTests/Problems/Problem0008Fixture.cs
@@ -0,0 +1,36 @@
+﻿using EulerLib.Problems;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EulerLibTests.Problems
+{
+    [TestFixture]
+    public class Problem0008Fixture
+    {
+        [TestCase("123", 1, 3)]
+        [TestCase("123", 2, 6)]
+        [TestCase("123459", 2, 45)]
+        [TestCase("593451", 3, 135)]
+        public void LargestProductOfDigitsTests(string text, int digits, int expectedResult)
+        {
+            var sut = new Problem0008();
+
+            var result = sut.LargestProductOfDigits(text, digits);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [TestCase("7", 7)]
+        [TestCase("123", 6)]
+        [TestCase("123459", 1080)]
+        [TestCase("593451", 2700)]
+        public void ProductOfDigitsTests(string text, int expectedResult)
+        {
+            var sut = new Problem0008();
+
+            var result = sut.ProductOfDigits(text);
+
+            result.Should().Be(expectedResult);
+        }
+    }
+}
