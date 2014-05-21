c06b737d9c81cc249218bd2f8b0d227991e6ada1
Ian Nelson <ian@ianfnelson.com>
2014-05-21 13:53:40 +0100

    Problem 21

--- EulerLib/Problems/Problem0021.cs
+++ EulerLib/Problems/Problem0021.cs
@@ -1,4 +1,8 @@
-﻿namespace EulerLib.Problems
+﻿using System.Collections.Generic;
+using System.Linq;
+using EulerLib.Extensions;
+
+namespace EulerLib.Problems
 {
     public class Problem0021 : IProblem
     {
@@ -7,16 +11,34 @@
             get { return 21; }
         }
 
-        public string Title { get; private set; }
+        public string Title { get { return "Amicable Numbers"; } }
 
         public string Solve()
         {
-            return null;
+            return SumOfAmicableNumbersBelow(10000).ToString();
+        }
+
+        public long SumOfAmicableNumbersBelow(int n)
+        {
+            var divisorSums = new Dictionary<long, long>();
+
+            for (var i = 2; i < n; i++)
+            {
+                divisorSums.Add(i, i.ProperDivisors().Sum());
+            }
+
+            var result =
+                divisorSums.Where(d => d.Value > 1 && d.Value <= n)
+                    .Where(
+                        a => divisorSums[a.Value] == a.Key && a.Key != a.Value)
+                    .Sum(x => x.Key);
+
+            return result;
         }
 
         public string Md5OfSolution
         {
-            get { return null; }
+            get { return "51e04cd4e55e7e415bf24de9e1b0f3ff"; }
         }
     }
 }
