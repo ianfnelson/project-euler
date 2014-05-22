ac3fc0eeb1320f7671efbd860cacdb072fa91023
Ian Nelson <ian@ianfnelson.com>
2014-05-22 13:30:42 +0100

    Problem 18

---
+++ EulerLib/Problems/Problem0018.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace EulerLib.Problems
+{
+    public class Problem0018 : IProblem
+    {
+        public int Id
+        {
+            get { return 18; }
+        }
+
+        public string Title
+        {
+            get { return "Maximum Path Sum I"; }
+        }
+
+        public string Solve()
+        {
+            var executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var filePath = Path.Combine(executingDirectory, "ContentFiles\\problem18.txt");
+            return MaximumPathThroughTriangle(filePath).ToString();
+        }
+
+        public long MaximumPathThroughTriangle(string filePath)
+        {
+            var triangle = ParseFile(filePath);
+
+            for (var row = triangle.Count-2; row >= 0; row--)
+            {
+                for (var cell = 0; cell < triangle[row].Length; cell++)
+                {
+                    triangle[row][cell] += Math.Max(triangle[row + 1][cell], triangle[row + 1][cell + 1]);
+                }
+            }
+
+            return triangle[0][0];
+        }
+
+        private List<int[]> ParseFile(string filePath)
+        {
+            return File.ReadLines(filePath)
+                       .Where(x => !string.IsNullOrWhiteSpace(x))
+                       .Select(ParseLine)
+                       .ToList();
+        }
+
+        private int[] ParseLine(string line)
+        {
+            return line.Split(' ').Select(int.Parse).ToArray();
+        }
+
+        public string Md5OfSolution
+        {
+            get { return "708f3cf8100d5e71834b1db77dfa15d6"; }
+        }
+    }
+}
---
+++ EulerLibTests/Problems/Problem0018Fixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using EulerLib.Problems;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EulerLibTests.Problems
+{
+    [TestFixture]
+    public class Problem0018Fixture
+    {
+        [Test]
+        public void MaxSumThroughSampleIs23()
+        {
+            var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestFiles\\problem18example.txt");
+
+            var sut = new Problem0018();
+
+            var result = sut.MaximumPathThroughTriangle(file);
+
+            result.Should().Be(23);
+        }
+    }
+}
