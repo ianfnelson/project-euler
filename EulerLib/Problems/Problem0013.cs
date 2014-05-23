64a295065d92a55d1e71645acb0382f7b81ccbdd
Ian Nelson <ian@ianfnelson.com>
2014-05-23 14:14:17 +0100

    Problem 13

---
+++ EulerLib/Problems/Problem0013.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+
+namespace EulerLib.Problems
+{
+    public class Problem0013 : IProblem
+    {
+        public int Id
+        {
+            get { return 13; }
+        }
+
+        public string Title
+        {
+            get { return "Large sum"; }
+        }
+
+        public string Solve()
+        {
+            var executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var filePath = Path.Combine(executingDirectory, "ContentFiles\\problem13.txt");
+
+            return SumBigIntegers(filePath).ToString().Substring(0,10);
+        }
+
+        public BigInteger SumBigIntegers(string filePath)
+        {
+            return File.ReadLines(filePath)
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(BigInteger.Parse)
+                .Aggregate(BigInteger.Zero, (current, value) => current + value);
+        }
+
+        public string Md5OfSolution
+        {
+            get { return "361113f19fd302adc31268f8283a4f2d"; }
+        }
+    }
+}
