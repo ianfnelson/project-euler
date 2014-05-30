7f85de5903a5a4b88d2ae27c1e487810fefee11d
Ian Nelson <ian@ianfnelson.com>
2014-05-30 15:09:50 +0100

    Problem 31

---
+++ EulerLib/Problems/Problem0031.cs
@@ -0,0 +1,43 @@
+﻿namespace EulerLib.Problems
+{
+    public class Problem0031 : IProblem
+    {
+        public int Id
+        {
+            get { return 31; }
+        }
+
+        public string Title
+        {
+            get { return "Coin sums"; }
+        }
+
+        private static readonly int[] CoinSizes = { 1, 2, 5, 10, 20, 50, 100, 200 };
+
+        public string Solve()
+        {
+            return WaysOfMaking(200).ToString();
+        }
+
+        public int WaysOfMaking(int target)
+        {
+            var ways = new int[target + 1];
+            ways[0] = 1;
+
+            for (int i = 0; i < CoinSizes.Length; i++)
+            {
+                for (int j = CoinSizes[i]; j <= target; j++)
+                {
+                    ways[j] += ways[j - CoinSizes[i]];
+                }
+            }
+
+            return ways[ways.Length - 1];
+        }
+
+        public string Md5OfSolution
+        {
+            get { return "142dfe4a33d624d2b830a9257e96726d"; }
+        }
+    }
+}
---
+++ EulerLibTests/Problems/Problem0031Fixture.cs
@@ -0,0 +1,25 @@
+﻿using EulerLib.Problems;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EulerLibTests.Problems
+{
+    [TestFixture]
+    public class Problem0031Fixture
+    {
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(5, 4)]
+        [TestCase(10, 11)]
+        public void WaysOfMakingTests(int n, int expectedWays)
+        {
+            var sut = new Problem0031();
+
+            var ways = sut.WaysOfMaking(n);
+
+            ways.Should().Be(expectedWays);
+        }
+    }
+}
