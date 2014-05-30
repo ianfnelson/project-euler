ef64f120d005331506ac1618856d96a941987f14
Ian Nelson <ian@ianfnelson.com>
2014-05-30 12:05:37 +0100

    Problem 30

---
+++ EulerLib/Problems/Problem0030.cs
@@ -0,0 +1,25 @@
+﻿namespace EulerLib.Problems
+{
+    public class Problem0030 : IProblem
+    {
+        public int Id
+        {
+            get { return 30; }
+        }
+
+        public string Title
+        {
+            get { return "Digit Fifth Powers"; }
+        }
+
+        public string Solve()
+        {
+            return new EulerLibFSharp.Problem0030().sumOfNumbersThatCanBeBeWrittenAsSumOfNthPower(5).ToString();
+        }
+
+        public string Md5OfSolution
+        {
+            get { return "27a1779a8a8c323a307ac8a70bc4489d"; }
+        }
+    }
+}
---
+++ EulerLibTests/Problems/Problem0030Fixture.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace EulerLibTests.Problems
+{
+    [TestFixture]
+    public class Problem0030Fixture
+    {
+        [TestCase(2, 2, 4)]
+        [TestCase(23, 2, 13)]
+        [TestCase(23, 3, 35)]
+        public void SumOfNthPowersOfDigitsTests(int x, int n, int expectedSum)
+        {
+            var sut = new EulerLibFSharp.Problem0030();
+
+            var sum = sut.sumOfNthPowersOfDigits(x, n);
+
+            sum.Should().Be(expectedSum);
+        }
+
+        [Test]
+        public void SumOfNumbersThatCanBeWrittenAsSumOfFourthPowerIs19316()
+        {
+            var sut = new EulerLibFSharp.Problem0030();
+
+            var sum = sut.sumOfNumbersThatCanBeBeWrittenAsSumOfNthPower(4);
+
+            sum.Should().Be(19316);
+        }
+    }
+}
