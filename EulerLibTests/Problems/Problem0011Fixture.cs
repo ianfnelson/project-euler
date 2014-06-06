f1a2198d709c2cabd94b6cc6b10bc48c6ac82d8d
Ian Nelson <ian@ianfnelson.com>
2014-06-06 12:05:48 +0100

    Problem 11

---
+++ EulerLibTests/Problems/Problem0011Fixture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using EulerLib.Problems;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EulerLibTests.Problems
+{
+    [TestFixture]
+    public class Problem0011Fixture
+    {
+        [TestCase("problem0011example2.txt", 2, 945)]
+        public void MaximumProductInAGrid_FindsHorizontals(string filename, int adjacentNumbers, int expectedResult)
+        {
+            DoMaximumProductInAGridTest(filename,adjacentNumbers, expectedResult);
+        }
+
+        [TestCase("problem0011example3.txt", 2, 1869)]
+        public void MaximumProductInAGrid_FindsVerticals(string filename, int adjacentNumbers, int expectedResult)
+        {
+            DoMaximumProductInAGridTest(filename, adjacentNumbers, expectedResult);
+        }
+
+        [TestCase("problem0011example1.txt", 2, 4005)]
+        public void MaximumProductInAGrid_FindsUphillDiagonals(string filename, int adjacentNumbers, int expectedResult)
+        {
+            DoMaximumProductInAGridTest(filename, adjacentNumbers, expectedResult);
+        }
+
+        [TestCase("problem0011example4.txt", 2, 4320)]
+        public void MaximumProductInAGrid_FindsDownhillDiagonals(string filename, int adjacentNumbers, int expectedResult)
+        {
+            DoMaximumProductInAGridTest(filename, adjacentNumbers, expectedResult);
+        }
+
+        private void DoMaximumProductInAGridTest(string filename, int adjacentNumbers,int expectedResult)
+        {
+            var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestFiles\\", filename);
+
+            var sut = new Problem0011();
+
+            var result = sut.MaximumProductInAGrid(file, adjacentNumbers);
+
+            result.Should().Be(expectedResult);
+        }
+    }
+}
