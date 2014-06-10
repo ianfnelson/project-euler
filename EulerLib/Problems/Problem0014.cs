ea97c0c73c41361fbdda117ce89fb62fd4d06c68
Ian Nelson <ian@ianfnelson.com>
2014-06-10 14:10:00 +0100

    Problem 14

---
+++ EulerLib/Problems/Problem0014.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EulerLib.Problems
+{
+    public class Problem0014 : IProblem
+    {
+        public int Id
+        {
+            get { return 14; }
+        }
+
+        public string Title
+        {
+            get { return "Longest Collatz sequence"; }
+        }
+
+        public string Solve()
+        {
+            var terms = CollatzChainTerms(1000000);
+            var maxValue = terms.Values.Max();
+
+            return terms.FirstOrDefault(x => x.Value == maxValue).Key.ToString();
+        }
+
+        public string Md5OfSolution
+        {
+            get { return "5052c3765262bb2c6be537abd60b305e"; }
+        }
+
+        public IDictionary<long, int> CollatzChainTerms(int maximumStartingNumber)
+        {
+            var dictionary = new Dictionary<long, int>();
+
+            for (var i = 2; i <= maximumStartingNumber; i++)
+            {
+                var termCount = 1;
+                var term = (long)i;
+
+                do
+                {
+                    if (dictionary.ContainsKey(term))
+                    {
+                        termCount += dictionary[term]-1;
+                        term = 1;
+                    }
+                    else if (term%2 == 0)
+                    {
+                        termCount++;
+                        term = term/2;
+                    }
+                    else
+                    {
+                        termCount ++;
+                        term = (3*term) + 1;
+                    }
+                } while (term != 1);
+
+                dictionary.Add(i, termCount);
+            }
+
+            return dictionary;
+        }
+    }
+}
---
+++ EulerLibTests/Problems/Problem0014Fixture.cs
@@ -0,0 +1,39 @@
+﻿using EulerLib.Problems;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EulerLibTests.Problems
+{
+    [TestFixture]
+    public class Problem0014Fixture
+    {
+        [TestCase(2)]
+        [TestCase(5)]
+        [TestCase(50)]
+        public void CollatzChainTerms_EntryForEachIntegerAboveTwo(int maximumStartingNumber)
+        {
+            // Arrange
+            var sut = new Problem0014();
+
+            // Act
+            var terms = sut.CollatzChainTerms(maximumStartingNumber);
+
+            // Assert
+            terms.Count.Should().Be(maximumStartingNumber - 1);
+        }
+
+        [TestCase(13,10)]
+        public void CollatzChainTerms_ContainsExpectedResults(int startingNumber, int expectedTerms)
+        {
+            // Arrange
+            var sut = new Problem0014();
+
+            // Act
+            var terms = sut.CollatzChainTerms(startingNumber);
+
+            // Assert
+            terms[startingNumber].Should().Be(expectedTerms);
+        }
+    
+    }
+}
