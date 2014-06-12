521e2f4ffc5e7b03dc85e021f542ae91ce5bac7e
Ian Nelson <ian@ianfnelson.com>
2014-06-12 11:34:18 +0100

    Problem 26

---
+++ EulerLib/Problems/Problem0026.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace EulerLib.Problems
+{
+    public class Problem0026 : IProblem
+    {
+        public int Id
+        {
+            get { return 26; }
+        }
+
+        public string Title
+        {
+            get { return "Reciprocal Cycles"; }
+        }
+
+        public string Solve()
+        {
+            return DenominatorWIthMaximumReciprocalCycleLength(1000).ToString();
+        }
+
+        public int DenominatorWIthMaximumReciprocalCycleLength(int maximumDenominator)
+        {
+            int maxCycleLength = 0;
+            int denominatorOfMaxCycleLength = 0;
+
+            for (int denominator = maximumDenominator; denominator > 0; denominator--)
+            {
+                if (maxCycleLength > denominator) break;
+
+                var cycleLength = ReciprocalCycleLength(denominator);
+
+                if (cycleLength > maxCycleLength)
+                {
+                    maxCycleLength = cycleLength;
+                    denominatorOfMaxCycleLength = denominator;
+                }
+            }
+
+            return denominatorOfMaxCycleLength;
+        }
+
+        public int ReciprocalCycleLength(int denominator)
+        {
+            int value = 1;
+            int position = 0;
+
+            var remainderPositions = new Dictionary<int, int>();
+
+            while (value !=0 && !remainderPositions.ContainsKey(value))
+            {
+                remainderPositions.Add(value, position);
+                value *= 10;
+                value %= denominator;
+                position++;
+            }
+
+            if (value == 0) return 0;
+
+            return position - remainderPositions[value];
+        }
+
+        public string Md5OfSolution
+        {
+            get { return "6aab1270668d8cac7cef2566a1c5f569"; }
+        }
+    }
+}
---
+++ EulerLibTests/Problems/Problem0026Fixture.cs
@@ -0,0 +1,39 @@
+﻿using EulerLib.Problems;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EulerLibTests.Problems
+{
+    [TestFixture]
+    public class Problem0026Fixture
+    {
+        [TestCase(2, 0)]
+        [TestCase(3, 1)]
+        [TestCase(4, 0)]
+        [TestCase(5, 0)]
+        [TestCase(6, 1)]
+        [TestCase(7, 6)]
+        [TestCase(8, 0)]
+        [TestCase(9, 1)]
+        [TestCase(10, 0)]
+        public void ReciprocalCycleLength_Tests(int denominator, int expectedCycleLength)
+        {
+            var sut = new Problem0026();
+
+            var cycleLength = sut.ReciprocalCycleLength(denominator);
+
+            cycleLength.Should().Be(expectedCycleLength);
+        }
+
+        [TestCase(4, 3)]
+        [TestCase(10, 7)]
+        public void MaximumReciprocalCycleLengthTest(int maximumDenomator, int expectedMaximumCycleLength)
+        {
+            var sut = new Problem0026();
+
+            var cycleLength = sut.DenominatorWIthMaximumReciprocalCycleLength(maximumDenomator);
+
+            cycleLength.Should().Be(expectedMaximumCycleLength);
+        }
+    }
+}
