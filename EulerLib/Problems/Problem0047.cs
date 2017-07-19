e4e04e438b94766856090bfab54000911a071e95
Ian Nelson <ian@iannelson.systems>
2017-07-19 21:56:44 +0100

    Problem 47

---
+++ EulerLib/Problems/Problem0047.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using EulerLib.Extensions;
+
+namespace EulerLib.Problems
+{
+    public class Problem0047 : IProblem
+    {
+        public int Id => 47;
+        public string Title => "Distinct Primes Factors";
+        public string Solve()
+        {
+            return FirstOfNConsecutiveIntegersWithNDistinctPrimeFactors(4).ToString();
+        }
+
+        public string Md5OfSolution => "748f517ecdc29106e2738f88aa7530f4";
+
+        public int DistinctPrimeFactors(int value)
+        {
+            var factors = value.PrimeFactors();
+
+            return factors.Distinct().Count();
+        }
+
+        public int FirstOfNConsecutiveIntegersWithNDistinctPrimeFactors(int n)
+        {
+            int? firstInSequence = null;
+            int valueUnderTest = 0;
+
+            do
+            {
+                valueUnderTest++;
+
+                var distinctPrimeFactorCount = DistinctPrimeFactors(valueUnderTest);
+
+                if (distinctPrimeFactorCount == n)
+                {
+                    if (firstInSequence == null)
+                    {
+                        firstInSequence = valueUnderTest;
+                    }
+                }
+                else
+                {
+                    firstInSequence = null;
+                }
+
+            } while (firstInSequence==null || valueUnderTest - firstInSequence != n-1);
+
+            return firstInSequence.Value;
+        }
+    }
+}
---
+++ EulerLibTests/Problems/Problem0047Fixture.cs
@@ -0,0 +1,36 @@
+﻿using EulerLib.Problems;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EulerLibTests.Problems
+{
+    [TestFixture]
+    public class Problem0047Fixture
+    {
+        [TestCase(14, 2)]
+        [TestCase(15, 2)]
+        [TestCase(644, 3)]
+        [TestCase(645, 3)]
+        [TestCase(646, 3)]
+        public void DistinctPrimeFactorCount(int x, int expectedValue)
+        {
+            var sut = new Problem0047();
+
+            var actual = sut.DistinctPrimeFactors(x);
+
+            actual.Should().Be(expectedValue);
+        }
+
+        [TestCase(2, 14)]
+        [TestCase(3, 644)]
+        [TestCase(4, 134043)]
+        public void FirstOfNConsecutiveIntegersWithNDistinctPrimeFactors(int n, int expectedValue)
+        {
+            var sut = new Problem0047();
+
+            var actual = sut.FirstOfNConsecutiveIntegersWithNDistinctPrimeFactors(n);
+
+            actual.Should().Be(expectedValue);
+        }
+    }
+}
