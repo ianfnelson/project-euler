8d765b73ccccf0c444431d5d346178a204c9ff9a
Ian Nelson <ian@iannelson.uk>
2023-12-03 20:20:07 +0000

    Problem 41

---
+++ EulerLib/Problems/Problem0041.cs
@@ -0,0 +1,22 @@
+using EulerLib.Extensions;
+using EulerLib.Sets;
+
+namespace EulerLib.Problems;
+
+public class Problem0041 : IProblem
+{
+    public int Id => 41;
+    public string Title => "Pandigital Prime";
+    public string Solve()
+    {
+        // 1 to n Pandigitals with 8 or 9 digits are always divisible by 3
+        return PandigitalNumbers
+            .Generate(1, 7)
+            .OrderDescending()
+            .Select(long.Parse)
+            .First(x => x.IsPrime())
+            .ToString();
+    }
+
+    public string Md5OfSolution => null;
+}
