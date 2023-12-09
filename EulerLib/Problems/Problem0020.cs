e1b62eba82ca821d46a88116ff8dd9207160f356
Ian Nelson <ian@iannelson.uk>
2023-12-09 20:25:21 +0000

    Problem 20

---
+++ EulerLib/Problems/Problem0020.cs
@@ -0,0 +1,23 @@
+using System.Numerics;
+using EulerLib.Extensions;
+
+namespace EulerLib.Problems;
+
+public class Problem0020 : IProblem
+{
+    public int Id => 20;
+    public string Title => "Factorial Digit Sum";
+    public string Solve()
+    {
+        var x = new BigInteger(1);
+
+        for (var i = 1; i <= 100; i++)
+        {
+            x = i * x;
+        }
+
+        return x.SumOfDigits().ToString();
+    }
+
+    public string Md5OfSolution { get; }
+}
