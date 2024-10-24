8271535395db4f5f39c0ca46e630e516dea493ec
Ian Nelson <ian@iannelson.uk>
2024-10-24 20:12:40 +0100

    tidying

--- EulerLibTests/SoakTestFixture.cs
+++ EulerLibTests/SoakTestFixture.cs
@@ -2,43 +2,42 @@
 using EulerLib;
 using EulerLib.Extensions;
 
-namespace EulerLibTests
+namespace EulerLibTests;
+
+[TestFixture]
+public class SoakTestFixture
 {
-    [TestFixture]
-    public class SoakTestFixture
+    [Test]
+    public void RunAllSolvedProblems()
     {
-        [Test]
-        public void RunAllSolvedProblems()
+        // Get All Problems
+        var problemType = typeof(IProblem);
+        var problems = Assembly
+            .GetAssembly(problemType)
+            .GetTypes()
+            .Where(problemType.IsAssignableFrom)
+            .Where(t => t.IsClass)
+            .OrderBy(x => x.Name)
+            .Select(x => (IProblem)Activator.CreateInstance(x));
+
+        var failures = new List<string>();
+
+        foreach (var problem in problems.Where(x => x.Md5OfSolution != null))
         {
-            // Get All Problems
-            var problemType = typeof(IProblem);
-            var problems = Assembly
-                .GetAssembly(problemType)
-                .GetTypes()
-                .Where(problemType.IsAssignableFrom)
-                .Where(t => t.IsClass)
-                .OrderBy(x => x.Name)
-                .Select(x => (IProblem)Activator.CreateInstance(x));
+            var solution = problem.Solve();
 
-            var failures = new List<string>();
-
-            foreach (var problem in problems.Where(x => x.Md5OfSolution != null))
+            var md5OfSolution = solution.ToMd5Hash();
+            if (!md5OfSolution.Equals(problem.Md5OfSolution))
             {
-                var solution = problem.Solve();
-
-                var md5OfSolution = solution.ToMd5Hash();
-                if (!md5OfSolution.Equals(problem.Md5OfSolution))
-                {
-                    failures.Add(
-                        string.Format("Problem {0} is not returning the expected solution (expected {1} found {2})",
-                            problem.Id, problem.Md5OfSolution, md5OfSolution));
-                }
+                failures.Add(
+                    string.Format("Problem {0} is not returning the expected solution (expected {1} found {2})",
+                        problem.Id, problem.Md5OfSolution, md5OfSolution));
             }
+        }
 
-            if (failures.Count != 0)
-            {
-                Assert.Fail(string.Join(Environment.NewLine, failures));
-            }
+        if (failures.Count != 0)
+        {
+            Assert.Fail(string.Join(Environment.NewLine, failures));
         }
     }
 }
