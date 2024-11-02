98867285a47873f91bbd1d030d6664cd8e2b7d3d
Ian Nelson <ian@iannelson.uk>
2024-11-02 22:32:13 +0000

    Problem 808

---
+++ EulerLib/Problems/0801_0900/Problem0808.cs
@@ -0,0 +1,42 @@
+using EulerLib.Extensions;
+using EulerLib.Sequences;
+
+namespace EulerLib.Problems._0801_0900;
+
+public class Problem0808 : IProblem
+{
+    public int Id => 808;
+    public string Title => "Reversible Prime Squares";
+
+    private readonly List<long> _primeSquaresList;
+    private readonly HashSet<string> _primeSquaresHashset;
+
+    public Problem0808()
+    {
+        _primeSquaresList = new PrimeNumbers()
+            .GenerateToMaximumValue(50000000)
+            .Select(x => x * x)
+            .ToList();
+        
+        _primeSquaresHashset = _primeSquaresList
+            .Select(x => x.ToString())
+            .ToHashSet();
+    }
+    public string Solve()
+    {
+        return _primeSquaresList
+            .Where(IsReversiblePrimeSquare)
+            .Take(50)
+            .Sum()
+            .ToString();
+    }
+
+    private bool IsReversiblePrimeSquare(long primeSquare)
+    {
+        return !primeSquare.IsPalindromic() &&
+               _primeSquaresHashset.Contains(primeSquare.ToString().ReverseString());
+    }
+    
+
+    public string Md5OfSolution => "";
+}
